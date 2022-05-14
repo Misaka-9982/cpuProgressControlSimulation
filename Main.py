41c4cb3e8dfdff2aa67dc73c5a00bcb4a4889710
Mikoto Misaka <1355016743@qq.com>
2022-05-14 16:36:49 +0800

    添加PCB类

--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication
-import sys, threading
+import sys
+import threading
 
 import Window
 
-if __name__ == '__main__':
+if __name__ == '__main__':     # mainThread
     app = QApplication(sys.argv)
     Mainwindow = QMainWindow()
     ui = Window.Ui_MainWindow()  # 创建ui对象
---
+++ PCB.py
@@ -0,0 +1,7 @@
+class PCB:
+    def __init__(self, progressname, runningtime, priority, status):
+        self.progressname = progressname
+        self.runningtime = runningtime
+        self.priority = priority
+        self.status = status
+        # 是否加入独立进程/同步进程
