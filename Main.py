4800a361e91783c6f0cb40d3851d19b952659757
Mikoto Misaka <1355016743@qq.com>
2022-05-21 11:56:43 +0800

    初步修复了ui闪烁的问题，修改了ui更新逻辑，每次更新前先置空表，再按照要刷新的行数设置行，再写入数据，防止删除时显示错误

--- Main.py
+++ Main.py
@@ -54,41 +54,48 @@ def uiupdatequeuedetect():
     if Global_var.Runningprocess is not None:
         temp_runningtime = Global_var.Runningprocess.runningtime
     while True:
+        # 刷新等待队列ui
         if len(Global_var.WaitingQueue) != temp_Wlen or Global_var.WaitingQueue != temp_W:
+            for i in range(temp_Wlen):  # 修改前先置空表
+                ui.WaitingQueue.removeRow(i)
             temp_W = Global_var.WaitingQueue
             temp_Wlen = len(Global_var.WaitingQueue)
-            ui.WaitingQueue.setRowCount(temp_Wlen)
-            if len(Global_var.WaitingQueue) == 0:
-                continue
+            ui.WaitingQueue.setRowCount(temp_Wlen)  # 先添加要更新的行数
             for n, i in enumerate(Global_var.WaitingQueue):
-                print('Waiting',i.processname)
+                print('Waiting', i.processname)
                 ui.WaitingQueue.setItem(n, 0, QTableWidgetItem(i.processname))
                 ui.WaitingQueue.setItem(n, 1, QTableWidgetItem(i.priority))
                 ui.WaitingQueue.setItem(n, 2, QTableWidgetItem(str(i.runningtime)))
                 ui.WaitingQueue.setItem(n, 3, QTableWidgetItem(str(i.memory)))
             ui.WaitingQueue.viewport().update()
+        # 刷新就绪队列ui
         if len(Global_var.ReadyQueue) != temp_Rlen or Global_var.ReadyQueue != temp_R:
+            for i in range(temp_Rlen):  # 修改前先置空表
+                ui.ReadyQueue.removeRow(i)
             temp_R = Global_var.ReadyQueue
             temp_Rlen = len(Global_var.ReadyQueue)
-            ui.ReadyQueue.setRowCount(temp_Rlen)
-            if len(Global_var.ReadyQueue) == 0:
-                continue
+            ui.ReadyQueue.setRowCount(temp_Rlen)  # 先添加要更新的行数
             for n, i in enumerate(Global_var.ReadyQueue):
                 ui.ReadyQueue.setItem(n, 0, QTableWidgetItem(i.processname))
                 ui.ReadyQueue.setItem(n, 1, QTableWidgetItem(i.priority))
                 ui.ReadyQueue.setItem(n, 2, QTableWidgetItem(str(i.runningtime)))
                 ui.ReadyQueue.setItem(n, 3, QTableWidgetItem(str(i.memory)))
             ui.ReadyQueue.viewport().update()
+        # 刷新运行中ui
         if Global_var.Runningprocess != temp_running:
+            ui.RunningQueue.removeRow(0)
             temp_running = Global_var.Runningprocess
+            ui.RunningQueue.setRowCount(1)
             ui.RunningQueue.setItem(0, 0, QTableWidgetItem(Global_var.Runningprocess.processname))
             ui.RunningQueue.setItem(0, 1, QTableWidgetItem(str(Global_var.Runningprocess.runningtime)))
             ui.RunningQueue.setItem(0, 2, QTableWidgetItem(str(Global_var.Runningprocess.memory)))
             ui.RunningQueue.viewport().update()
+        # 刷新运行时间ui
         if Global_var.Runningprocess is not None and Global_var.Runningprocess.runningtime != temp_runningtime:
             temp_runningtime = Global_var.Runningprocess.runningtime
             ui.RunningQueue.setItem(0, 1, QTableWidgetItem(str(temp_runningtime)))
             ui.RunningQueue.viewport().update()
+
 
 if __name__ == '__main__':     # mainThread
     app = QApplication(sys.argv)
